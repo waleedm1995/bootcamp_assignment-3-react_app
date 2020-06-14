282e6b51233c6bf13ad4ba4d4d7d780b4670efa7
waleedm1995 <waleedmalikw@yahoo.com>
2020-06-14 12:45:52 +0500

    domain name in main.yaml updated

---
+++ src/waleed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Waleed(props) {
+    return (
+        <div>
+            <p>I love playing <b>{props.game}</b></p>
+            <p>I play as an <b>{props.position}</b></p>
+
+        </div>
+    )
+}
+
+export default Waleed;
